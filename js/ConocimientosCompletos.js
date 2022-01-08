7eec7e3e657bbb2ef84ba841d26b05502572f386
DavHD09 <davidhdz99@hotmail.com>
2022-01-08 15:13:37 -0500

    codigo optimizado

--- js/ConocimientosCompletos.js
+++ js/ConocimientosCompletos.js
@@ -1,11 +1,11 @@
 function abrirmenu() {
-    document.getElementById("filaultima").style.display = "block"
-    document.getElementById("ampliar").style.display = "none";
-    document.getElementById("cerrar").style.display = "inline"
+    document.getElementById("tra-fila-skills").style.display = "block"
+    document.getElementById("btn-abrir").style.display = "none";
+    document.getElementById("btn-cerrar").style.display = "inline"
 }
 
 function cerrarmenu() {
-    document.getElementById("filaultima").style.display = "none"
-    document.getElementById("ampliar").style.display = "inline";
-    document.getElementById("cerrar").style.display = "none"
+    document.getElementById("tra-fila-skills").style.display = "none"
+    document.getElementById("btn-abrir").style.display = "inline";
+    document.getElementById("btn-cerrar").style.display = "none"
 }
